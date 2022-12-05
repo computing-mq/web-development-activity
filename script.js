fa1ec0a746aabbf6772f9fdd2bf58537c93aa2c2
Steve Cassidy <steve.cassidy@mq.edu.au>
2022-12-06 10:31:40 +1100

    Add to the notes and exercises.
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ window.onload = () => {
     console.log(
         '%cHello there! Welcome to the developer tools.',
         "font-size: large; font-style: italic ;padding: 2px")
+
+    console.log(
+        "%cWe'll use these tools to explore how the web works.",
+        "font-style: italic; color: red;"
+        )
 }
